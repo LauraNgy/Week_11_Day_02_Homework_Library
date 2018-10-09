38e8aec3ab0f8a4e6330beb9cd116711a293ff89
Laura Nagy <ngylaura@gmail.com>
2018-10-09 17:55:41 +0100

    adds booksByGenre hash to Library

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,11 +1,14 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Library {
+    private HashMap<String, Integer> booksByGenre;
     private ArrayList<Book> books;
     private int capacity;
 
     public Library(int capacity) {
        this.books = new ArrayList<>();
+       this.booksByGenre = new HashMap<>();
         this.capacity = capacity;
     }
 
@@ -15,6 +18,15 @@ public class Library {
 
     public void addBook(Book book) {
         if ( this.capacity > bookCount() ) {
+            String genre = book.getGenre();
+            if (booksByGenre.containsKey(genre)) {
+                int bookCount = booksByGenre.get(genre);
+                bookCount ++;
+                booksByGenre.put(genre, bookCount);
+            }
+            else {
+                booksByGenre.put(genre, 1);
+            }
             this.books.add(book);
         }
     }
@@ -26,5 +38,9 @@ public class Library {
 
     public void lend(Book book) {
         this.books.remove(book);
+    }
+
+    public HashMap getBooksByGenre() {
+        return this.booksByGenre;
     }
 }
--- src/test/java/LibraryTest.java
+++ src/test/java/LibraryTest.java
@@ -16,8 +16,8 @@ public class LibraryTest {
         book1 = new Book("A game of thrones", "George R. R. Martin", "fantasy");
         book2 = new Book("A clash of kings", "George R. R. Martin", "fantasy");
         book3 = new Book("A storm of swords", "George R. R. Martin", "fantasy");
-        book4 = new Book("A feast for crows", "George R. R. Martin", "fantasy");
-        book5 = new Book("A dance with dragons", "George R. R. Martin", "fantasy");
+        book4 = new Book("A feast for crows", "George R. R. Martin", "fiction");
+        book5 = new Book("A dance with dragons", "George R. R. Martin", "fiction");
         library = new Library(4);
     }
 
@@ -53,6 +53,16 @@ public class LibraryTest {
         library.addBook(book2);
         library.lend(book2);
         assertEquals(1, library.bookCount());
+    }
+
+    @Test
+    public void booksByGenre() {
+       library.addBook(book1);
+       library.addBook(book2);
+       library.addBook(book5);
+        assertEquals(2, library.getBooksByGenre().get(book1.getGenre()));
+        assertEquals(2, library.getBooksByGenre().get(book2.getGenre()));
+        assertEquals(1, library.getBooksByGenre().get(book5.getGenre()));
     }
 
 }
